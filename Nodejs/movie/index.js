81efa711875f79a8aa871638bb8082962a58a73b
willianwt2000 <willianmetelien@gmail.com>
2025-03-25 20:07:28 -0400

    fixin a cors problem

--- Nodejs/movie/index.js
+++ Nodejs/movie/index.js
@@ -31,24 +31,52 @@ app.get('/movies/:id', (req, res) => {
 //----------------------------------------------------
 
 app.post('/movies', (req, res) => {
-
   const result = validateMovie(req.body)
-  if (result.error) {
-    return res.status(400).json({error: Json.parse(result.error.message)})
+
+  if (!result.success) {
+    // 422 Unprocessable Entity
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
   }
- 
 
-
-
+  // en base de datos
   const newMovie = {
-    id:crypto.randomUUID(),//uuid id -- aleatorio
+    id: crypto.randomUUID(), // uuid v4
     ...result.data
   }
+
+  // Esto no sería REST, porque estamos guardando
+  // el estado de la aplicación en memoria
   movie.push(newMovie)
-  res.status(201).json(req.body);
+  console.log(newMovie)
+
+  res.status(201).json(newMovie)
 })
 
 
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movie.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const updateMovie = {
+    ...movie[movieIndex],
+    ...result.data
+  }
+
+  movie[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
+})
 
 
 //------------------------------------------
